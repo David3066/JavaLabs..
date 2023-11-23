d3d365922258e697c9cd8ff9b8c5e49663c4f86a
David <Hongd2174@gmail.com>
2023-11-23 09:39:32 +0700

    first

--- recursion.java
+++ recursion.java
@@ -1,15 +1,15 @@
-public class recursion {
-  public static void main(String[] args) {
-
-    //call your recursion fuction
-    //print result
-
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+public class Recursion {
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
     }
-  
+
+    public static void main(String[] args) {
+        int n = 10;
+        int fibonacciNumber = fibonacci(n);
+        System.out.println("The " + n + "th Fibonacci number is: " + fibonacciNumber);
+    }
 }
-    
